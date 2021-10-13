08d2a6b5ddc299563d2e84b507ccb81ef925ba17
Nico Rithner <nicorithner@gmail.com>
2021-10-12 19:54:13 -0600

    Complete lesson 78

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -46,8 +46,11 @@ export class TasksController {
   }
 
   @Delete('/:id')
-  async deleteTask(@Param('id') id: string): Promise<void> {
-    this.tasksService.deleteTask(id);
+  async deleteTask(
+    @Param('id') id: string,
+    @GetUser() user: User,
+  ): Promise<void> {
+    this.tasksService.deleteTask(id, user);
   }
 
   @Patch('/:id/status')
